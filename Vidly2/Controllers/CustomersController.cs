24a2fcdeab7ba97fa634daec821744af35464e2a
RomanOrlovskiy <romanorlovskiy92@gmail.com>
2016-08-31 00:46:23 +0300

    Section 2 Exercise

---
+++ Vidly2/Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customers = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customers == null)
+                return HttpNotFound();
+
+            return View(customers);
+        }
+
+        private List<Customer> GetCustomers()
+        {
+            var customers = new List<Customer>()
+            {
+                new Customer {Name = "John Smith", Id = 1},
+                new Customer {Name = "Mary Williams", Id = 2 }
+            };
+
+            return customers;
+        }
+
+    }
+}
--- Vidly2/Controllers/MoviesController.cs
+++ Vidly2/Controllers/MoviesController.cs
@@ -10,42 +10,69 @@ namespace Vidly2.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        public ActionResult Index()
         {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
+
+            var movies = GetMovies();
+            
+            return View(movies);
+        }
+
+        private List<Movie> GetMovies()
+        {
+            var movies = new List<Movie>()
             {
-                new Customer {Name = "Customer 1" },
-                new Customer {Name = "Customer 2" }
+                new Movie {Name = "Shrek", Id = 1},
+                new Movie {Name = "Wall-e", Id = 2 }
             };
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
+            return movies;
+        }
+        public ActionResult Details(int id)
+        {
+            var movies = GetMovies().SingleOrDefault(m => m.Id == id);
 
-            return View(viewModel);
+            if (movies == null)
+                return HttpNotFound();
+
+            return View(movies);
         }
         public ActionResult Edit(int id)
         {
             return Content("id=" + id);
         }
 
+        //// GET: Movies/Random
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Shrek!" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer {Name = "Customer 1" },
+        //        new Customer {Name = "Customer 2" }
+        //    };
+
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
+
+        //    return View(viewModel);
+        //}
         // /movies
         //If pageIndex is not specified we dispaly the first page.
         //If sortBy is null, we sort by name.
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
