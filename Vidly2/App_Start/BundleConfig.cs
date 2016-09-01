e3393502f36d149d88dc1653f5f6e19399634dac
RomanOrlovskiy <romanorlovskiy92@gmail.com>
2016-09-01 15:30:21 +0300

    Enable client-side validation

--- Vidly2/App_Start/BundleConfig.cs
+++ Vidly2/App_Start/BundleConfig.cs
@@ -11,6 +11,19 @@ namespace Vidly2
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            #region Enabling Client-side validation
+            //All files that match the pattern "~/Scripts/jquery.validate*"
+            //will be combined and compressed and they will be available
+            //at the adress "~/bundles/jqueryval". This adress is used in
+            //"section scripts" to anable client-side validation in CustomerForm view.
+            //Client-side validations only works with standart data annotations like
+            //required, stringlength, range, etc. Attribute like Min18YearsIfAMember
+            //will require additional jQuery code to be able to catch it client-side.
+            //But in this case, if there would be a need to change something in the logic
+            //then you would need to change the server-side AND client-side code.
+            //Thats why it is better to validate bussiness logic purely on the server
+            //while client-side validation takes care of some simple stuff.
+            #endregion
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
