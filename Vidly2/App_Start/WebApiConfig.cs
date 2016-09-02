f72288104c4c314eb6645d137a3351b9a3a5dd27
RomanOrlovskiy <romanorlovskiy92@gmail.com>
2016-09-02 09:52:54 +0300

    Use Camel Notation

--- Vidly2/App_Start/WebApiConfig.cs
+++ Vidly2/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -9,8 +11,24 @@ namespace Vidly2
     {
         public static void Register(HttpConfiguration config)
         {
+            /*
+             Pascal notation - first leter of each word(property) is uppercase
+             Camel notation - first leter is lower case.
+             Json file uses Pascal notation, but JavaScript uses camel one.
+             Here is how to fix it:
+             */
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
+            //Now when you check with Postman, you will recieve a banch
+            //of customers with lower-case first letter properties.
+
+
             config.MapHttpAttributeRoutes();
 
+            //Here we dont have action routes as in usual routing pattern.
+            //like : routeTemplate: "/controller/action/{id}",
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
