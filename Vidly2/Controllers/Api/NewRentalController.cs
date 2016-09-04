47815f2941cb2cb73e3e6647d63874590ca49c88
RomanOrlovskiy <romanorlovskiy92@gmail.com>
2016-09-04 14:12:01 +0300

    Begin to implement Rental feature: Create new API and Domain Model

---
+++ Vidly2/Controllers/Api/NewRentalController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using Vidly2.Models;
+using Vidly2.Dtos;
+
+namespace Vidly2.Controllers.Api
+{
+    
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/newrental/
+        public IHttpActionResult NewRental()
+        {
+
+
+            return Ok();
+        }
+
+        // POST /api/createrental
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult CreateRental(NewRentalDto newRental)
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ Vidly2/Models/Rental.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidly2.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "DateTime2")]
+        public DateTime DateRented { get; set; }
+
+        [Column(TypeName = "DateTime2")]
+        public DateTime? DateReturned { get; set; }
+
+        [Required]
+        public Movie Movie { get; set; }
+
+        [Required]
+        public Customer Customer { get; set; }
+        
+
+    }
+}
